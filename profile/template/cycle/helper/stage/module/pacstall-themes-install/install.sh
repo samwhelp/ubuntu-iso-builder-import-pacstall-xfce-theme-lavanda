0ff29a84b262a7a78cd9739c87caaa1eb68b1c15
samwhelp <samwhelp@users.noreply.github.com>
2025-07-28 17:32:41 +0800

    fix

--- profile/template/cycle/helper/stage/module/pacstall-themes-install/install.sh
+++ profile/template/cycle/helper/stage/module/pacstall-themes-install/install.sh
@@ -132,15 +132,9 @@ sys_pacstall_install_packages () {
 local run_cmd=$(cat << __EOF__
 	pacstall -QPINs
 		bean-appearance-citrus-icon-theme
-		bean-appearance-vimix-gtk-theme
-		bean-appearance-vimix-kde-theme
 
-		bean-appearance-orchis-gtk-theme
-		bean-appearance-graphite-gtk-theme
-		bean-appearance-fluent-gtk-theme
-		bean-appearance-colloid-gtk-theme
-		bean-appearance-layan-gtk-theme
-		bean-appearance-qogir-gtk-theme
+		bean-appearance-lavanda-gtk-theme
+		bean-appearance-lavanda-kde-theme
 
 __EOF__
 )
